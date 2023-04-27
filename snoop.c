63ff664cf1d73f557664e03781f685600923aa18
mskogen <theskogen@gmail.com>
2023-04-26 19:16:10 -0600

    Added daemon functionality to snoop and updated start-stop script to launch snoop daemon

--- snoop.c
+++ snoop.c
@@ -19,18 +19,94 @@
 *               http server and log frame data to an external drive.
 ​*/
 #include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
 #include <string.h>
 #include <errno.h>
 #include <syslog.h>
 #include <time.h>
-#include "http_client.h"
+#include <signal.h>
 
-// TOREMOVE: only for sleep to temporarily test log
-#include <unistd.h>
+#include "http_client.h"
 
 #define LOG_FILE_BASE   ("snoop_log")
 #define MAX_PATH_SIZE   (256)
 #define MAX_LINE_SIZE   (2048)
+
+// Global variables
+bool exit_status = false;
+bool syslog_open = false;
+bool logfile_open = false;
+FILE *logfile = NULL;
+// pthread_mutex_t thread_mutex;
+// bool mutex_active = false;
+
+// Handles both SIGINT and SIGTERM signals
+static void signal_handler(int signum)
+{
+    if ((signum == SIGINT) || (signum == SIGTERM)) {
+        syslog(LOG_INFO, "Caught signal, exiting\n");
+
+        // Set global to shutdown daemon when possible
+        exit_status = true;
+        return;
+    }
+    
+    // Unknown signal
+    syslog(LOG_ERR, "Error: Caught unknown signal\n");
+    return;
+}
+
+// Cleanup connections before closing
+void cleanup(bool terminate)
+{
+    // int status = 0;
+
+    // If we are exiting after this call, close all open file descriptors
+    if (terminate) {
+
+        // if (timer_active) {
+        //     status = timer_delete(timer);
+        //     if (status != 0) {
+        //         syslog(LOG_ERR, "Error timer_delete(): %s\n", strerror(errno));
+        //     }
+        //     timer_active = false;
+        // }
+
+        // if (mutex_active) {
+        //     status = pthread_mutex_destroy(&thread_mutex);
+        //     if (status != 0) {
+        //         syslog(LOG_ERR, "Error pthread_mutex_destroy(): %s\n", strerror(status));
+        //     }
+        //     mutex_active = false;
+        // }
+
+        // if (tmp_file_exists) {
+        //     status = remove(TMP_FILE);
+        //     if (status != 0) {
+        //         syslog(LOG_ERR, "Error remove(): %s\n", strerror(errno));
+        //     }
+        //     tmp_file_exists = false;
+        // }
+
+        // Close out connection to the file descriptor if file was opened
+        if (logfile_open) {
+            fclose(logfile);
+            logfile_open = false;
+        }
+
+        // Close out connection to system logger
+        if (syslog_open) {
+            closelog();
+            syslog_open = false;
+        }
+    }
+
+    // Set exit status in case it hasn't been set already
+    exit_status = true;
+
+    return;
+}
 
 int main(int argc, char**argv)
 {
@@ -38,7 +114,41 @@ int main(int argc, char**argv)
     if (argc != 2) {
         printf("ERROR: Invalid number of arguments %i\n", argc);
         printf("Usage: ./snoop /path/to/storage/drive\n");
-        return 1;
+        return EXIT_FAILURE;
+    }
+
+    // Open connection to system logger with default LOG_USER facility
+    openlog("snoop", LOG_CONS, LOG_USER);
+    syslog_open = true;
+
+    if (signal(SIGINT, signal_handler) == SIG_ERR) {
+        syslog(LOG_ERR, "Error: Cannot register SIGINT\n");
+        cleanup(true);
+        return EXIT_FAILURE;
+    }
+
+    if (signal(SIGTERM, signal_handler) == SIG_ERR) {
+        syslog(LOG_ERR, "Error: Cannot register SIGTERM\n");
+        cleanup(true);
+        return EXIT_FAILURE;
+    }
+
+    // Create daemon process and close initial start process
+    pid_t pid = fork();
+    switch (pid) {
+    case -1:
+        // Failed to create child process
+        syslog(LOG_ERR, "Error fork()\n");
+        cleanup(true);
+        return EXIT_FAILURE;
+    case 0:
+        // Inside child process
+        syslog(LOG_DEBUG, "Successfully created child process()\n");
+        break;
+    default:
+        // Close initial parent process
+        cleanup(true);
+        return EXIT_FAILURE;
     }
 
     // Objects for timestamping application start
@@ -56,35 +166,30 @@ int main(int argc, char**argv)
     info = gmtime(&timestamp);
     strftime(time_buffer, sizeof(time_buffer), "%m%d%Y_%H%M%S", info);
 
-    // Open connection to system logger with default LOG_USER facility
-    openlog("snoop", LOG_CONS, LOG_USER);
-
     // First argument is a path to directory on filesystem
     char write_file[MAX_PATH_SIZE];
     snprintf(write_file, sizeof(write_file), "%s/%s_%s", 
                 argv[1], LOG_FILE_BASE, time_buffer);
 
-    // printf("Write to file '%s'\n", write_file);
-    // closelog();
-    // exit(0);
-
     /* 
      * Open a connection to the file, create new file if it doesn't exist and
      * overwrite file if it does exist.
      */
-    FILE *p_file;
     int ret_val = 0;
-    p_file = fopen(write_file, "w");
+    logfile = fopen(write_file, "w");
 
-    if (p_file == NULL) {
+    if (logfile == NULL) {
         syslog(LOG_ERR, "Failed to open %s: %m\n", write_file);
-        return 1;
+        cleanup(true);
+        return EXIT_FAILURE;
+    } else {
+        logfile_open = true;
     }
 
     // File opened succesfully, write to file
     snprintf(write_str, sizeof(write_str), "%s - %s\n", 
                 "Starting Snoop", time_buffer);
-    ret_val = fprintf(p_file, "%s", write_str);
+    ret_val = fprintf(logfile, "%s", write_str);
 
     if (ret_val < 0) {
         // Error writing to file, check errno
@@ -96,9 +201,6 @@ int main(int argc, char**argv)
         // Successful write
         syslog(LOG_DEBUG, "Successfully wrote full string to file.\n");
     }
-
-    // TOREMOVE: Sleep for 10 seconds
-    sleep(10);
 
     // struct sockaddr_in addr, cl_addr;  
     // int sockfd, ret; 
@@ -189,6 +291,7 @@ int main(int argc, char**argv)
 
     // openFile();
 
+    sleep(60);
     
     // Zero out our buffers
     memset(time_buffer, 0, sizeof(time_buffer));
@@ -202,7 +305,7 @@ int main(int argc, char**argv)
     // Write to file
     snprintf(write_str, sizeof(write_str), "%s - %s\n", 
                 "Closing Snoop", time_buffer);
-    ret_val = fprintf(p_file, "%s", write_str);
+    ret_val = fprintf(logfile, "%s", write_str);
 
     if (ret_val < 0) {
         // Error writing to file, check errno
@@ -215,13 +318,8 @@ int main(int argc, char**argv)
         syslog(LOG_DEBUG, "Successfully wrote full string to file.\n");
     }
 
-    // Close out connection to the file descriptor if file was opened
-    if (p_file) {
-        fclose(p_file);
-    }
+    // Cleanup any opened resources
+    cleanup(true);
 
-    // Close out connection to system logger
-    closelog();
-
-    return 0;
+    return EXIT_SUCCESS;
 }
